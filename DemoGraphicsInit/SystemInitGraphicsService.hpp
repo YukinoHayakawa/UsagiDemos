32a79bb741f674db9645dc8c12a42bff8542865d
Yukino Hayakawa <tennencoll@gmail.com>
2021-08-30 22:16:07 +0800

    graphics init demo

---
+++ DemoGraphicsInit/SystemInitGraphicsService.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <Usagi/Entity/EntityDatabase.hpp>
+#include <Usagi/Module/Common/Color/Color.hpp>
+#include <Usagi/Runtime/Service.hpp>
+
+#include <Usagi/Module/Platform/Vulkan/VulkanGpuDevice.hpp>
+#include <Usagi/Module/Service/Graphics/Enum.hpp>
+#include <Usagi/Module/Service/Windowing/ServiceNativeWindowManager.hpp>
+
+namespace usagi
+{
+USAGI_DECL_ALIAS_SERVICE(
+    ServiceHardwareGraphics,
+    LazyInitService<VulkanGpuDevice>
+);
+
+struct SystemInitGraphicsService
+{
+    using WriteAccess = C<>;
+    using ReadAccess = C<>;
+
+    void update(auto &&rt, auto &&db)
+    {
+        // touch the service so it will be initialized.
+        (void)USAGI_SERVICE(rt, ServiceHardwareGraphics);
+    }
+};
+}
