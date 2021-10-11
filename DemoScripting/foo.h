095412ca577815d559e5ab9837ca1de9d4333ca7
Yukino Hayakawa <tennencoll@gmail.com>
2021-10-11 18:46:47 +0800

    add demoscripting

---
+++ DemoScripting/foo.h
@@ -0,0 +1,6 @@
+int foo()
+{
+    return 12345;
+}
+
+// clang -cc1 foo.h -emit-pch -o foo.pch
---
+++ DemoScripting/main.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+// #define NDEBUG
+
+#include <iostream>
+
+#include <fmt/ostream.h>
+
+#include <Usagi/Modules/Common/ProgramModule/ClangJIT.hpp>
+#include <Usagi/Modules/Common/ProgramModule/RuntimeModule.hpp>
+
+using namespace usagi;
+
+std::string source = R"(
+int bar()
+{
+    return foo();
+}
+)";
+
+int main(int argc, char *argv[])
+{
+    ClangJIT jit;
+
+    auto compiler = jit.create_compiler();
+    compiler.set_pch("foo.pch");
+    MemoryRegion src;
+    src.base_address = source.data();
+    src.length = source.size();
+    compiler.add_source("test", src);
+    auto module_ = compiler.compile();
+    auto ret = module_->get_function_address<int()>("bar")();
+
+    std::cout << ret << std::endl;
+}
