97d36f1a5769f57fa2749ed47943b10b620452b4
Yukino Hayakawa <tennencoll@gmail.com>
2021-10-30 19:08:26 +0800

    scripting demo

---
+++ DemoScripting/Script.cpp
@@ -0,0 +1,49 @@
+﻿#include "Database.hpp"
+
+using namespace usagi;
+
+Archetype<
+    TestComponent1
+> gArchetype1;
+
+Archetype<
+    TestComponent2
+> gArchetype2;
+
+enum ResumeCondition : std::uint8_t
+{
+    NEVER,
+    USER_INPUT,
+    NEXT_FRAME,
+};
+
+std::pair<std::uint64_t, ResumeCondition>
+script_main(std::uint64_t entry, auto &&db) {
+    switch(entry)
+    {
+        case 0:
+        {
+            gArchetype1(C<TestComponent1>()).num = 123456;
+            db.insert(gArchetype1);
+            gArchetype1(C<TestComponent1>()).num = 567890;
+            db.insert(gArchetype1);
+            return { 1, NEXT_FRAME };
+        }
+        case 1:
+        {
+            for(auto &&e : db.view(C<TestComponent1>()))
+                if(component<TestComponent1>(e).num == 123456)
+                    e.destroy();
+            gArchetype2(C<TestComponent2>()).flt = 123.456f;
+            return { 2, NEXT_FRAME };
+        }
+        case 2:
+        {
+            for(auto &&e : db.view(C<TestComponent1>()))
+                printf("%llu\n", component<TestComponent1>(e).num);
+            for(auto &&e : db.view(C<TestComponent2>()))
+                printf("%f\n", component<TestComponent2>(e).flt);
+        }
+        default: return { -1, NEVER };
+    }
+}
---
+++ DemoScripting/main2.cpp
@@ -0,0 +1,123 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <future>
+#include <map>
+#include <iostream>
+
+#include <Usagi/Runtime/Task.hpp>
+#include <Usagi/Runtime/TaskExecutor.hpp>
+#include <Usagi/Runtime/Service/SimpleService.hpp>
+#include <Usagi/Modules/Runtime/Executive/AppHost.hpp>
+#include <Usagi/Modules/Runtime/Executive/SystemTaskList.hpp>
+#include <Usagi/Modules/Common/Math/ComponentRegion2D.hpp>
+#include <Usagi/Modules/Common/Time/Clock.hpp>
+#include <Usagi/Modules/Runtime/Asset/Package/AssetPackageFilesystem.hpp>
+#include <Usagi/Modules/Runtime/Executive/ServiceStateTransitionGraph.hpp>
+
+#include "Database.hpp"
+
+using namespace usagi;
+
+class StdTaskExecutor : public TaskExecutor
+{
+    std::map<std::uint64_t, std::future<void>> mTask;
+    using TaskRef = decltype(mTask)::iterator;
+    std::mutex mMutex;
+    std::uint64_t mTaskId = 0;
+
+public:
+    std::uint64_t submit(
+        std::unique_ptr<Task> task,
+        std::optional<std::vector<std::uint64_t>> wait_on) override
+    {
+        std::lock_guard lk(mMutex);
+
+        auto task_id = ++mTaskId;
+
+        auto future = std::async(
+            std::launch::async,
+            [t = std::move(task), wait = std::move(wait_on), this]() {
+            Clock clk;
+            // using namespace std::chrono_literals;
+            // std::this_thread::sleep_for(5ms);
+
+            if(wait.has_value())
+            {
+                for(auto &&w : *wait)
+                {
+                    LockGuard lk(mMutex);
+                    auto wt = mTask.find(w);
+                    assert(wt != mTask.end());
+                    lk.unlock();
+                    wt->second.wait();
+                }
+            }
+
+            if(!t->precondition()) throw std::runtime_error("");
+            t->on_started();
+            t->run();
+            t->on_finished();
+            if(!t->postcondition()) throw std::runtime_error("");
+
+            std::cout << clk.realtime_elapsed() << std::endl;
+        }
+        );
+
+        mTask.emplace(task_id, std::move(future));
+
+        return task_id;
+    }
+};
+
+using ServiceAsyncWorker = SimpleService<StdTaskExecutor>;
+
+#include <Usagi/Modules/Gameplay/Scripting/SystemInvokeScriptCoroutine.hpp>
+
+struct Services
+    : ServiceStateTransitionGraph
+    , ServiceAsyncWorker
+    , ServiceAssetManager
+    , ServiceJitCompilation
+{
+    Services()
+    {
+    }
+};
+
+using TaskList = SystemTaskList<
+    SystemInvokeScriptCoroutine
+>;
+
+using App = AppHost<
+    Services,
+    TaskList,
+    // the following two were not included by the script executioner
+    C<  ComponentCoroutineContinuation,
+        ComponentSecondaryAssetRef,
+        TestComponent1,
+        TestComponent2
+    >
+>;
+
+using Components = App::EnabledComponents;
+
+static_assert(std::is_same_v<Components, AppDbTraits::EnabledComponents>);
+
+int main(int argc, char *argv[])
+{
+    const auto data_folder = "demo_script";
+    std::filesystem::remove_all(data_folder);
+
+    App app { data_folder };
+    auto &svc = app.services();
+
+    // allow SystemInvokeScriptCoroutine to find the script from current folder
+    auto &assets = USAGI_SERVICE(svc, ServiceAssetManager);
+    assets.add_package(std::make_unique<AssetPackageFilesystem>("."));
+
+    auto &tg = USAGI_SERVICE(app.services(), ServiceStateTransitionGraph);
+    while(!tg.should_exit)
+    {
+        app.update();
+    }
+}
